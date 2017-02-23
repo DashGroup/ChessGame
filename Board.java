cdb2dbf98632d6733d7b528898f1980f851bdc1c
DashGroup <1527537681@qq.com>
2017-02-23 15:14:24 +0800

    Add files via upload

---
+++ Board.java
@@ -0,0 +1,5 @@
+public class Board
+{
+    private Player playerWhite;
+    private Player playerBlack;
+}
---
+++ Knight.java
@@ -0,0 +1,12 @@
+public class Knight extends Pieces
+{
+    public boolean rules(){
+        return true;
+    }
+    public boolean specialRules(){
+        return true;
+    }
+    public boolean checkRules(){
+        return true;
+    }
+}
