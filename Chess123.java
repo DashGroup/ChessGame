4612a60abedc45cbaac6e9deb44044cbfe85cb13
LiuXinTongTracy <playerdongdong@163.com>
2017-02-23 14:25:20 +0800

    Add files via upload

---
+++ Chess123.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class Chess123 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Chess123
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Chess123
+     */
+    public Chess123()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
