afcd42a8e5473df0dc5667c3e4e057155d53e1c6
DashGroup <1527537681@qq.com>
2017-02-23 15:28:52 +0800

    Add files via upload

---
+++ Queen.java
@@ -0,0 +1,12 @@
+public class Queen extends Pieces
+{
+    public boolean rules(){
+        return true;
+    }
+    public boolean specialRules(){
+        return true;
+    }
+    public boolean checkRules(){
+        return true;
+    }
+}
