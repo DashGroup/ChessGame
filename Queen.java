e83c4a3284fa1d7e405d9755da82cf11986af77a
DashGroup <1527537681@qq.com>
2017-02-24 07:49:31 +0800

    Revert "1"
    This reverts commit 9a97250575a8b2a4238af47dc4870e05d960f050.

---
+++ Queen.java
@@ -0,0 +1,20 @@
+public class Queen extends Chesspieces
+{
+    private String[] name;
+    private String[] position;
+    private boolean[] alive;
+    private boolean[] ifMove;
+    private int numCount;
+    public boolean rules(){
+        return true;
+    }
+    public boolean specialRules(){
+        return true;
+    }
+    public boolean checkRules(){
+        return true;
+    }
+    public boolean ifAlive(){
+        return true;
+    }
+}
