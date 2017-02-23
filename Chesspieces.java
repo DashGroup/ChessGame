dbe71549ffe74c061d5dd90cd90a2ff89be16840
DashGroup <1527537681@qq.com>
2017-02-23 13:15:45 +0800

    Update Chesspieces.java

--- Chesspieces.java
+++ Chesspieces.java
@@ -2,5 +2,6 @@ public abstract class Chesspieces
 {
     public abstract boolean rules();
     public abstract boolean specialRules();
+    public abstract boolean checkRules();
     public abstract boolean ifAlive();
 }
