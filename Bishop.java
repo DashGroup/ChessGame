80eeaaf82e7ea23f4145a2b5b4a93082c8564f9e
DashGroup <1527537681@qq.com>
2017-03-17 11:16:58 +0800

    Add files via upload

--- Bishop.java
+++ Bishop.java
@@ -1,23 +1,39 @@
 public class Bishop extends Pieces
 {   
-    public boolean rules(int[] command){
+    //if moving diagonally
+    public boolean followsBasicRules(int[] command){
         int distanceUpAndDown =  Math.abs(command[0]- position[0]);
         int distanceLeftAndRight =  Math.abs(command[1]- position[1]);
-        if(distanceUpAndDown==distanceLeftAndRight){return testBlockOfBishop(command);}
+        if(distanceUpAndDown==distanceLeftAndRight){return isBlocked(command);}
         return false;
     }
-    private boolean testBlockOfBishop(int command[])
-    {
+    private int calculateUnitRow(int command[]){
         int unitRow;
+        if(command[0]>position[0]){
+            unitRow=-1;
+        }
+        else{
+            unitRow=1;
+        }
+        return unitRow;
+    }
+    private int calculateUnitColumn(int command[]){
         int unitColumn;
-        if(command[0]>position[0]){unitRow=-1;}
-        else{unitRow=1;}
-        if(command[1]>position[1]){unitColumn=-1;}
-        else{unitColumn=1;}
+        if(command[1]>position[1]){
+            unitColumn=-1;
+        }
+        else{
+            unitColumn=1;
+        }
+        return unitColumn;
+    }
+    private boolean isBlocked(int command[]){
+        int unitRow = calculateUnitRow(command);
+        int unitColumn = calculateUnitColumn(command);
         boolean checkForReturn=true;
         int row=command[0];
         int column=command[1];
-        for(int i=0;i<Math.abs(position[0]-command[0])-1;i++){
+        for(int i = 0;i<Math.abs(position[0]-command[0])-1;i++){
             row=row+unitRow;
             column=column+unitColumn;
             if(Board.getBoard()[row][column]!=null){
@@ -27,14 +43,8 @@ public class Bishop extends Pieces
         }
         return checkForReturn;
     }
-    public boolean ifAllowedToMove(int[] command){
-        boolean check=false;
-        if(rules(command)){check=true;}
-        if(exceptionalRules(command)){check = true;}
-        if(check==true){movementNumCount++;}
-         return check;
-    }
-    public boolean exceptionalRules(int[] command){
+    // no exceptional rules
+    public boolean followsExceptionalRules(int[] command){
         return false;
     }
     public void setProperty(String input,String name,Pieces piece,String color){
