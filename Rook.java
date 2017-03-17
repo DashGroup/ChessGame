a0b6e97bddcdc307da96459da976ef450a03213b
DashGroup <1527537681@qq.com>
2017-03-17 15:30:35 +0800

    Add files via upload

--- Rook.java
+++ Rook.java
@@ -3,25 +3,36 @@ public class Rook extends Pieces
     //check if the command is correct to move up down or right left
     public boolean followsBasicRules(int[] command){
         boolean check = false;
-        if(command[1]==position[1]){check = isNotBlockedByAnotherPieceUpOrDown(command);}
-        else if(command[0]==position[0]){check = isNotBlockedByAnotherPieceRightOrLeft(command);}
+        if(command[1]==position[1]){
+            check = isNotBlockedByAnotherPieceUpOrDown(command);
+        }
+        else if(command[0]==position[0]){
+            check = isNotBlockedByAnotherPieceRightOrLeft(command);
+        }
         return check;
+    }
+    private int calculateUnitIndex(int[] command, int i){
+        if(command[i]<position[i]){
+            return -1;
+        }
+        else{
+            return 1;
+        }
     }
     private boolean isNotBlockedByAnotherPieceUpOrDown(int[] command){//No block ==> true
         boolean check = false;
-        int addIndex = 0;
-        if(command[0]<position[0]){addIndex = -1;}
-        else{addIndex = 1;}
+        int addIndex = calculateUnitIndex(command,0);
         check = checkBlockedHelperUpOrDown(command,addIndex);
         return check;
     }
     private boolean isNotBlockedByAnotherPieceRightOrLeft(int[] command){
         boolean check = false;
-        int addIndex = 0;
-        if(command[1]>position[1]){addIndex = 1;}
-        else{addIndex = -1;}
+        int addIndex = calculateUnitIndex(command,1);
         check = checkBlockedHelperRightOrLeft(command,addIndex);
         return check;
+    }
+    private boolean isEnemy(int[] command){
+        return Board.getBoard()[command[0]][command[1]]!=null&&!Board.getBoard()[command[0]][command[1]].color.equals(color);
     }
     //return true if no blocked or when tested to the end index to check if the blocked one is with different color
     private boolean checkBlockedHelperUpOrDown(int[] command, int addIndex){
@@ -35,8 +46,9 @@ public class Rook extends Pieces
             }
             if(index==command[0]){
                 check = false;
-                if(Board.getBoard()[command[0]][command[1]]!=null&&
-                !Board.getBoard()[command[0]][command[1]].color.equals(color)){checkForReturn = true;}
+                if(isEnemy(command)){
+                    checkForReturn = true;
+                }
             }
             index+=addIndex;
         }
@@ -53,8 +65,9 @@ public class Rook extends Pieces
             }
             if(index==command[1]){
                 check = false;
-                if(Board.getBoard()[command[0]][command[1]]!=null&&
-                !Board.getBoard()[command[0]][command[1]].color.equals(color)){checkForReturn = true;}    
+                if(isEnemy(command)){
+                    checkForReturn = true;
+                }    
             }
             index+=addIndex;
         }
